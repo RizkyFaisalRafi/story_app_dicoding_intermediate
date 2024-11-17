4497fa9657a4fbc17284fc6055f233439ff2d5f8
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-11-17 11:24:23 +0700

    + Add Theme

---
+++ lib/presentation/design_system/constants/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Typography Font Size
+const double sizeExtraSmall = 12.0;
+const double sizeSmall = 14.0;
+const double sizeMedium = 16.0;
+const double sizeLarge = 18.0;
+const double sizeExtra = 20.0;
+const double sizeExtraLarge = 22.0;
+
+// Typography Font Weight
+const FontWeight weightLight = FontWeight.w300;
+const FontWeight weightReguler = FontWeight.w400;
+const FontWeight weightMedium = FontWeight.w500;
+const FontWeight weightSemiBold = FontWeight.w600;
+const FontWeight weightBold = FontWeight.w700;
+
+// Typography Font Type
+final TextStyle fontPoppins = GoogleFonts.poppins();
+
+// Default Margin
+const double defaultMargin = 16.0;
