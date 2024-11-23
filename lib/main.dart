8d82b6ff9c98c6e2c0bca7510563ab53181bd898
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-11-23 15:03:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:story_app_dicoding_intermediate/data/data_sources/remote/auth_remote_datasource.dart';
 import 'package:story_app_dicoding_intermediate/presentation/view/provider/login_provider.dart';
 import 'package:story_app_dicoding_intermediate/presentation/view/provider/register_provider.dart';
-import 'package:story_app_dicoding_intermediate/presentation/view/ui/login_page.dart';
+import 'package:http/http.dart' as http;
+import 'data/repository/login_repository_impl.dart';
+import 'presentation/router/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +16,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appRouter = AppRouter();
+    final router = appRouter.router;
+    final httpClient = http.Client();
+    final remoteDataSource = AuthRemoteDatasourceImpl(client: httpClient);
+    final repository = LoginRepositoryImpl(remoteDataSource: remoteDataSource);
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => LoginProvider(),
+          create: (_) => LoginProvider(authRepository: repository),
         ),
         ChangeNotifierProvider(
           create: (_) => RegisterProvider(),
         ),
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         title: 'Story App Dicoding Intermidiate',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const LoginPage(),
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
+        routeInformationProvider: router.routeInformationProvider,
+        debugShowCheckedModeBanner: false,
+        // home: const LoginPage(),
       ),
     );
   }
