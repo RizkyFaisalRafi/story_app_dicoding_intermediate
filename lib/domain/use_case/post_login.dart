eea69db1742e04094ab3bb3b85c572b958a84d8c
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-11-26 22:34:39 +0700

    Update post_login.dart

--- lib/domain/use_case/post_login.dart
+++ lib/domain/use_case/post_login.dart
@@ -1,15 +1,14 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:story_app_dicoding_intermediate/domain/repositories/auth_repository.dart';
-
 import '../../common/error/failure.dart';
-import '../entities/login_params.dart';
 import '../entities/login_result.dart';
 
 class PostLogin {
   final AuthRepository authRepository;
 
-  PostLogin(this.authRepository,);
+  PostLogin(
+    this.authRepository,
+  );
 
   // Menjalankan proses login
   Future<Either<Failure, LoginResult>> execute(String email, String password) {
