e01c9a0a78719c655eaa55db606c2313bfcafc4a
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-12-03 06:56:44 +0700

    Create error_state_widget.dart

---
+++ lib/presentation/design_system/widgets/error_state_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class ErrorStateWidget extends StatelessWidget {
+  final String message;
+
+  const ErrorStateWidget({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    if (message.contains("Failed to connect to the network")) {
+      return Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                'assets/lottie/no_connection.json',
+                width: 300,
+                height: 300,
+              ),
+              Text(
+                'Tidak Ada Koneksi Internet!',
+                // style: GoogleFonts.robotoCondensed(
+                //   fontSize: 16,
+                //   color: Colors.black,
+                //   fontWeight: FontWeight.w400,
+                // ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                'assets/lottie/error.json',
+                width: 300,
+                height: 300,
+              ),
+              Text(
+                'Terjadi Kesalahan Pada Server!',
+                // style: GoogleFonts.robotoCondensed(
+                //   fontSize: 16,
+                //   color: Colors.black,
+                //   fontWeight: FontWeight.w400,
+                // ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
