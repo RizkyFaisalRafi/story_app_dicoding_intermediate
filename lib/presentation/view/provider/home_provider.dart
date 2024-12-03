d287b1dbc2460f4174b1a69b6a4f0201ff494507
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-12-03 07:01:09 +0700

    Add Layer Presentation for List Story API Get All Stories HomePage

---
+++ lib/presentation/view/provider/home_provider.dart
@@ -0,0 +1,84 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:story_app_dicoding_intermediate/domain/entities/list_story.dart';
+import 'package:story_app_dicoding_intermediate/domain/use_case/get_all_story.dart';
+
+import '../../../common/error/failure.dart';
+import '../../../common/state_enum.dart';
+
+class HomeProvider extends ChangeNotifier {
+  final GetAllStory getAllStory;
+  var _listStory = <ListStory>[];
+  List<ListStory> get listStory => _listStory;
+
+  String _errorMessage = '';
+  String? get errorMessage => _errorMessage;
+
+  RequestState _state = RequestState.empty;
+  RequestState get state => _state;
+
+  HomeProvider({required this.getAllStory});
+
+  Future<void> fetchAllStory(BuildContext context, String token) async {
+    final listStory = await getAllStory.execute(token);
+    _state = RequestState.loading;
+    notifyListeners();
+
+    try {
+      listStory.fold(
+        (failure) {
+          _state = RequestState.error;
+          _errorMessage = failure.message;
+          log('Home Provider: $_errorMessage');
+
+          // Cek tipe kegagalan dan berikan pesan kesalahan yang lebih spesifik
+          if (failure is ServerFailure) {
+            if (failure.message.contains('Missing authentication')) {
+              _errorMessage = 'Anda belun Log In';
+            } else {
+              log(failure.message);
+              _errorMessage = 'Gagal mengambil data story';
+            }
+          } else if (failure is ConnectionFailure) {
+            _errorMessage =
+                'Tidak dapat terhubung ke server. Periksa koneksi internet Anda! $failure';
+          } else {
+            _errorMessage = 'Terjadi kesalahan saat mengambil data story';
+          }
+          notifyListeners();
+
+          // Tampilkan pesan error ke pengguna
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(_errorMessage),
+            backgroundColor: Colors.red,
+          ));
+        },
+        (data) {
+          _state = RequestState.loaded;
+          _listStory = data;
+          notifyListeners();
+
+          // Tampilkan pesan sukses
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Sukses Get Data'),
+            backgroundColor: Colors.green,
+          ));
+        },
+      );
+    } catch (e) {
+      _state = RequestState.error;
+      _errorMessage = "Kesalahan tidak terduga: $e";
+      log('Unexpected error: $e');
+      notifyListeners();
+
+      // Pesan default untuk error yang tidak diketahui
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Terjadi kesalahan tidak terduga: $e"),
+          backgroundColor: Colors.red,
+        ));
+      }
+    }
+  }
+}
--- lib/presentation/view/ui/home_page.dart
+++ lib/presentation/view/ui/home_page.dart
@@ -1,13 +1,142 @@
+import 'dart:developer';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:story_app_dicoding_intermediate/common/state_enum.dart';
+import 'package:story_app_dicoding_intermediate/presentation/design_system/constants/theme.dart';
+import 'package:story_app_dicoding_intermediate/presentation/view/provider/home_provider.dart';
+
+import '../../design_system/widgets/error_state_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final String queryToken;
+  const HomePage({
+    super.key,
+    required this.queryToken,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final homeProvider = Provider.of<HomeProvider>(context, listen: false);
+
+    // Panggil fetchAllStory saat halaman pertama kali dibuka
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        homeProvider.fetchAllStory(
+          context,
+          queryToken,
+        );
+      },
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      body: Stack(
+        children: [
+          // Latar Belakang Blur
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+            child: Container(
+              color: Colors.grey.withOpacity(0.1), // Lapisan semi-transparan
+            ),
+          ),
+
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                backgroundColor: const Color(0xffFC6A67),
+                centerTitle: true,
+                title: Text(
+                  'Home Page',
+                  style: fontPoppins.copyWith(
+                    fontSize: 24,
+                    fontWeight: weightSemiBold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Consumer<HomeProvider>(
+                builder: (context, provider, _) {
+                  final stories = provider.listStory;
+                  final stateData = provider.state;
+                  if (stateData == RequestState.error) {
+                    log(provider.errorMessage ?? 'Null Bray');
+                    return ErrorStateWidget(
+                      message: provider.errorMessage ?? 'Null',
+                    );
+                  } else if (stateData == RequestState.loading) {
+                    return const SliverToBoxAdapter(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  } else if (stories.isEmpty) {
+                    return const SliverToBoxAdapter(
+                      child: Center(
+                        child: Text('No stories available'),
+                      ),
+                    );
+                  } else if (stateData == RequestState.loaded) {
+                    return SliverList.builder(
+                      itemCount: stories.length,
+                      itemBuilder: (context, index) {
+                        final story = stories[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 8,
+                            horizontal: 12,
+                          ),
+                          child: ListTile(
+                            leading: SizedBox(
+                              width: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.network(
+                                  fit: BoxFit.cover,
+                                  story.photoUrl,
+                                ),
+                              ),
+                            ),
+                            title: Text(story.name),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Description: ${story.description}',
+                                  maxLines: 1,
+                                ),
+                                Text(
+                                  'Id: ${story.id}',
+                                  maxLines: 1,
+                                ),
+                                Text(
+                                  'Created at: ${story.createdAt}',
+                                  maxLines: 1,
+                                ),
+                                Text(
+                                  'Latitude: ${story.lat}',
+                                  maxLines: 1,
+                                ),
+                                Text(
+                                  'Longitude: ${story.lon}',
+                                  maxLines: 1,
+                                ),
+                              ],
+                            ),
+                            onTap: () {},
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const SliverToBoxAdapter(
+                      child: Text('Undefined State'),
+                    );
+                  }
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
