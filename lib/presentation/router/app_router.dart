b64c16467d6520625e6ccbc2b9a7d8d00e46b961
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-12-05 00:26:10 +0700

    Add Routing SplashPage and AddStoryGuestPage

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,6 +1,6 @@
-import 'dart:developer';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:story_app_dicoding_intermediate/presentation/view/ui/add_story_guest_page.dart';
+import 'package:story_app_dicoding_intermediate/presentation/view/ui/splash_page.dart';
 import '../view/ui/home_page.dart';
 import '../view/ui/login_page.dart';
 import '../view/ui/register_page.dart';
@@ -9,41 +9,67 @@ import 'route_constants.dart';
 class AppRouter {
   final router = GoRouter(
     debugLogDiagnostics: true, // Log Route
-    initialLocation: RouteConstants.loginPath, // First Screen
+    initialLocation: RouteConstants.splashPath, // First Screen
     routes: [
+      // Splash Page
+      GoRoute(
+        name: RouteConstants.splash,
+        path: RouteConstants.splashPath,
+        builder: (context, state) => const SplashPage(),
+      ),
+
       // Login Page
       GoRoute(
-          name: RouteConstants.login,
-          path: RouteConstants.loginPath,
-          builder: (context, state) => const LoginPage(),
-          routes: [
-            // Register Page
-            GoRoute(
-              name: RouteConstants.register,
-              path: RouteConstants.registerPath,
-              builder: (context, state) => const RegisterPage(),
-            ),
-          ]),
+        name: RouteConstants.login,
+        path: RouteConstants.loginPath,
+        builder: (context, state) => const LoginPage(),
+        routes: [
+          // Register Page
+          GoRoute(
+            name: RouteConstants.register,
+            path: RouteConstants.registerPath,
+            builder: (context, state) => const RegisterPage(),
+          ),
+        ],
+      ),
 
       // Home Page
       GoRoute(
         name: RouteConstants.home,
         path: RouteConstants.homePath,
         builder: (context, state) {
-          final token = state.extra as String?;
-          log('Received token in HomePage: $token');
+          // final token = state.extra as String?;
+          // log('Received token in HomePage: $token');
 
-          if (token == null || token.isEmpty) {
-            return const Scaffold(
-              body: Center(
-                child: Text('Error: Token is not found!'),
-              ),
-            );
-          }
-          return HomePage(
-            queryToken: token,
-          );
+          // if (token == null || token.isEmpty) {
+          //   return Scaffold(
+          //     body: Center(
+          //       child: Column(
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: [
+          //           const Text('Error: Token is not found!'),
+          //           ElevatedButton(
+          //             onPressed: () {
+          //               // Menghapus data otentikasi dari penyimpanan lokal
+          //               AuthLocalDatasourceImpl().deleteToken();
+          //             },
+          //             child: const Text('Keluar Akun'),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   );
+          // }
+          return const HomePage(
+              // queryToken: token,
+              );
         },
+      ),
+
+      GoRoute(
+        name: RouteConstants.addStoryGuest,
+        path: RouteConstants.addStoryGuestPath,
+        builder: (context, state) => const AddStoryGuestPage(),
       ),
     ],
   );
--- lib/presentation/router/route_constants.dart
+++ lib/presentation/router/route_constants.dart
@@ -1,4 +1,8 @@
 class RouteConstants {
+  /// route for page [SplashPage]
+  static const String splash = 'splash_';
+  static const String splashPath = '/splash_';
+
   /// route for page [LoginPage]
   static const String login = 'login_';
   static const String loginPath = '/login_';
@@ -10,4 +14,8 @@ class RouteConstants {
   /// route for [HomePage]
   static const String home = 'home_';
   static const String homePath = '/home_';
+
+  /// route for page [AddStoryGuestPage]
+  static const String addStoryGuest = 'add_story_guest_';
+  static const String addStoryGuestPath = '/add_story_guest_';
 }
